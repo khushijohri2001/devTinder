6ea3c57f1d8525616d5605e66e9bbcdc4bc86085
Khushi Johri <khushijohri2001@gmail.com>
2025-05-01 16:48:38 +0530

    get user's all connections api

--- server/src/router/user.js
+++ server/src/router/user.js
@@ -4,31 +4,50 @@ const User = require("../models/user");
 const { userAuth } = require("../middleware/auth");
 const ConnectionRequest = require("../models/connectionRequest");
 
+const USER_SAFE_STRING = "firstName lastName photoUrl age gender";
+
 // GET - All Users data
-// userRouter.get('/user/connections', async(req, res) => {
+userRouter.get('/user/connections', userAuth, async(req, res) => {
 
-//     try{
-//         const users = await User.find({});
+    try{
+        const loggedInUser = req.user;
 
-//         if(!users){
-//             res.status(404).send("User not found")
-//         } else{
-//             res.send(users, "User found!")
-//         }
-//     }catch(err){
-//         res.status(400).send("Something went wrong")
-//     }
-// })
+        const connectionRequest = await ConnectionRequest.find({
+            $or:[
+                { fromUserId: loggedInUser._id, status: "accepted"},
+                { toUserId: loggedInUser._id, status: "accepted"}
+            ]
+        })
+        .populate("fromUserId", USER_SAFE_STRING)
+        .populate("toUserId", USER_SAFE_STRING);
+
+        const data = connectionRequest.map((request) => {
+            // Can't compare two object id, we need to convert them into string
+            if(request.fromUserId._id.toString() === loggedInUser._id.toString() ){
+                
+                return request.toUserId
+            } else{
+                return request.fromUserId;
+            }
+            
+        } )
+       
+        res.send(data)
+       
+    }catch(err){
+        res.status(400).send("ERROR: ", err.message)
+    }
+})
 
 // Get all the pending connection request for the loggedin user
-userRouter.post("/user/requests/pending", userAuth, async (req, res) => {
+userRouter.get("/user/requests/pending", userAuth, async (req, res) => {
   try {
     const loggedInUser = req.user;
 
     const connectionRequest = await ConnectionRequest.find({
       toUserId: loggedInUser._id,
       status: "interested",
-    }).populate("fromUserId", "firstName lastName photoUrl age gender");
+    }).populate("fromUserId", USER_SAFE_STRING);
 
     //get only the list of users not other details = array of pending connection requests
     const data = connectionRequest.map((request) => request.fromUserId)
