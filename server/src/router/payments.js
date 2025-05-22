c880582e2b5957de67ca2b2def60d9dbb0d36a63
Khushi Johri <khushijohri2001@gmail.com>
2025-05-22 16:15:42 +0530

    updates

---
+++ server/src/router/payments.js
@@ -0,0 +1,25 @@
+// const express = require('express');
+// const paymentRouter = express.Router();
+
+// paymentRouter.post('/payment/create', userAuth, async(req, res) => {
+//     try{
+//         const order = await razorpayInstance.orders.create({
+//             amount: "70000",
+//             currency: "INR",
+//             receipt: "receipt #1",
+//             notes: {
+//                 firstName,
+//                 lastName,
+//                 emailId,
+//                 membershipType: "Monthly"
+//             }
+//         });
+
+//         console.log(order);
+        
+
+//         // const {status, } = order;
+//     }catch(err){
+        
+//     }
+// })
