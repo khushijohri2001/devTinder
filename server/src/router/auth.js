4002bd9b0d885d9182c0f7ce6786f5fdfd2911fe
Khushi Johri <khushijohri2001@gmail.com>
2025-07-30 19:01:08 +0530

    test

--- server/src/router/auth.js
+++ server/src/router/auth.js
@@ -83,6 +83,9 @@ authRouter.post("/login", async (req, res) => {
 
       res.cookie("token", token, {
         expires: new Date(Date.now() + 8 * 3600000),
+        httpOnly: true,
+        secure: true,
+        sameSite: "None",
       });
 
       res.send(existingUser);
